8150360fe649089e4a1b6ee9e1829138db9d3cbe
garrygit config --user.email newby1979@gmail.com <69111078+090221979@users.noreply.github.com>
2025-05-15 11:48:13 +0100

    changes

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const postCSSPlugins = [
+    require("postcss-import"),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+module.exports = 
+{
+    entry:"./app/assets/scripts/App.js",
+    output:{
+        filename:"bundled.js",
+        path:path.resolve(__dirname,'app')
+    },
+    devServer:{
+        watchFiles:["app/**/*.html"],
+        static:{
+            directory:path.join(__dirname,"app"),
+            watch:false
+        },
+        hot:true,//allows webpack to inject the new CSS and JS into the browsers memory on the fly without reload or refresh
+        port:5500,
+        liveReload:false
+    },
+    mode:"development",
+    module:{
+        rules:[
+            {
+                test:/\.css$/i,
+                use: ["style-loader", { loader: "css-loader", options: { url: false } }, { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]            }
+        ]
+    }
+}
