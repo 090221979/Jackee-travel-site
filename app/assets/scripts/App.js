f24ef5c25c97532cc730f741487b81417606ad05
garrygit config --user.email newby1979@gmail.com <69111078+090221979@users.noreply.github.com>
2025-07-09 09:41:59 +0100

    Complted desktop and mobile styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,14 @@
 import'../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
+
+
 
 if(module.hot)
     {
         module.hot.accept()
     }
+
+/* my work for lesson 41 on webpack by brad schiff */
+
