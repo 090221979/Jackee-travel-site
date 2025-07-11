11e75de36a4f4516d8176474edd68074c51277a8
garrygit config --user.email newby1979@gmail.com <69111078+090221979@users.noreply.github.com>
2025-07-11 11:23:54 +0100

    completed modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,37 @@ import'../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/stickHeader';
+// import Modal from './modules/Modal';
 
 
-let mobileMenu = new MobileMenu();
+new MobileMenu(); //only save to a vairable if you need access to the methods later 
 new RevealOnScroll(document.querySelectorAll(".feature-item"),75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"),60);
-let stickyHeader = new StickyHeader();
+new StickyHeader();
+// new Modal();
+let modal //used to store the instance of the imported class called Modal
 
+document.querySelectorAll('.open-modal').forEach(el =>
+    {//when the button is clicked the modal variable is checked for its type and then the class is loaded if needed
+        el.addEventListener('click', e=>
+            {
+                e.preventDefault()
+                if(typeof modal == 'undefined')
+                    {
+                    import(/*webpackChunkName: "modal"*/'./modules/Modal')
+                    .then((x)=>
+                        {
+                            modal = new x.default()//default() is used because of the way webpack imports files
+                            setTimeout(()=>{modal.opentheModal()},20)
+                        })
+                    .catch(()=> console.log("there was a problem"))
+                    }
+                    else
+                    {
+                        modal.opentheModal()
+                    }
+            })
+    })
 
 if(module.hot)
     {
@@ -17,3 +41,6 @@ if(module.hot)
 
 /* my work for lesson 41 on webpack by brad schiff */
 
+/*store the instances of a class in a variable if your different
+  modules would need to interact with each other and for 
+  complex projects look up an NPM file called EventEmitter */
